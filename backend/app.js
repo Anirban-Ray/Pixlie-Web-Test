e24662f6306567891b55ba5c986cb1c36fdb5078
rayanirbancms <rayanirbancms@gmail.com>
2019-01-15 20:07:40 +0530

    Project upload

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,135 @@
 const express = require('express');
-const mysql= require('mysql');
+const path = require('path');
+const mysql = require('mysql');
+const cors = require('cors')
 const app = express();
+app.use(cors());
 const dbConfig = require('./config/connection')
 var connection = mysql.createConnection(dbConfig);
-
 connection.connect();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/request', function(req, res) {
-  console.log('received message from knowlarity get method');
-  console.log(req.query);
-  res.status(200).send({
-    message: 'success'
+app.post('/hero', function(req, res) {
+      console.log('received request');
+      var heroName = "Hero" + Math.floor(Date.now() / 1000);
+      var sql = "INSERT INTO hero (name,status,fightNo,win_score,lose_score) VALUES ('"+heroName+"','Alive',0,0,0);";
+      connection.query(sql, function (err, result) {
+        if (err) {
+          console.log(err);
+          res.status(200).send({
+          message: 'Error while performing Query.'
+          });
+        }else{
+          var name=Math.floor(Math.random() * 10);
+                      // imgURL: "http://localhost:3000/public/"+name+".png",
+          res.status(200).send({
+            heroName: heroName,
+            imgURL: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/11_avatar-512.png",
+            message: 'success'
+          });
+        }
+      });
+});
+app.post('/fight', function(req, res) {
+    console.log('received request');
+    connection.query("SELECT * FROM hero WHERE status='Alive'", function (err, result, fields) {
+       if (err){
+         console.log(err);
+       }else {
+          if(result.length>1){
+            var hero1 = result[Math.floor(Math.random()*result.length)];
+            var hero2 = result[Math.floor(Math.random()*result.length)];
+            while(hero1.name===hero2.name){
+              hero2 = result[Math.floor(Math.random()*result.length)];
+            }
+            res.status(200).send({
+              hero1Name:hero1.name,
+              hero12Name:hero2.name,
+              imgURL: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/11_avatar-512.png",
+              message: 'success'
+            });
+          }else{
+            res.status(200).send({
+              message: 'Only one hero alive.'
+            });
+          }
+       }
+
+    });
+});
+app.get('/winner', function(req, res) {
+  console.log('received request');
+  connection.query("SELECT * FROM hero WHERE name='"+req.query.winner_name+"'", function (err, result, fields) {
+     if (err){
+       console.log(err);
+     }else {
+       connection.query("UPDATE hero SET fightNo= "+(parseInt(result[0].fightNo)+1)+",win_score="+(parseInt(result[0].win_score)+1)+" WHERE name='"+req.query.winner_name+"'", function (err, result, fields) {
+          if (err){
+            console.log(err);
+          }else {
+            connection.query("SELECT * FROM hero WHERE name='"+req.query.loser_name+"'", function (err, result, fields) {
+               if (err){
+                 console.log(err);
+               }else {
+                 connection.query("UPDATE hero SET status='Dead', fightNo= "+(parseInt(result[0].fightNo)+1)+",lose_score="+(parseInt(result[0].lose_score)+1)+" WHERE name='"+req.query.loser_name+"'", function (err, result, fields) {
+                    if (err){
+                      console.log(err);
+                    }else {
+                      res.status(200).send({
+                        message: 'success'
+                      });
+                    }
+                 });
+               }
+
+            });
+          }
+       });
+     }
+
+  });
+});
+app.post('/heroList', function(req, res) {
+  console.log("received request");
+  connection.query("SELECT * FROM hero WHERE status='Alive'", function (err, result, fields) {
+     if (err){
+       console.log(err);
+     }else {
+          res.status(200).send({
+            result:result,
+            imgURL: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/11_avatar-512.png",
+            message: 'success'
+          });
+     }
+
+  });
+});
+app.post('/champion', function(req, res) {
+  console.log("received request");
+  connection.query("SELECT * FROM hero WHERE status='Alive'", function (err, result, fields) {
+     if (err){
+       console.log(err);
+     }else {
+       console.log(result.length);
+       if(result.length>1){
+         res.status(200).send({
+           length:result.length,
+           message: 'unsuccess'
+         });
+       }else{
+         res.status(200).send({
+           name:result[0].name,
+           message: 'success'
+         });
+       }
+
+     }
+
   });
 });
 module.exports = app;
---
+++ frontend/vue/src/components/HelloWorld.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <v-btn color="success"  @click="hero">
+      New Hero
+    </v-btn>
+    <v-btn color="error"  @click="fight">
+      fight To Death
+    </v-btn>
+    <v-btn color="warning"  @click="champion">
+      Champion
+    </v-btn>
+    <div class="row">
+      <div class="col-4">
+        List of Heros
+        <div v-for="(img,index) in images"  >
+          <img width="100px" v-bind:src="img"><br/>
+          <span>{{heroName[index]}}</span>
+        </div>
+      </div>
+      <div class="col-4">
+        Fight
+        <div class="">
+          <img width="100px" v-bind:src="fighterImage1">
+          <span>{{hero1}}</span>
+        </div>
+        <div class="">
+          <img width="100px" v-bind:src="fighterImage2">
+          <span>{{hero2}}</span>
+        </div>
+        <span>{{fightText}}</span>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    data: () => ({
+        images:[],
+        heroName:[],
+        hero1:"",
+        hero2:"",
+        fighterImage1:"",
+        fighterImage2:"",
+        fightText:"",
+        timer:"",
+
+    }),
+    mounted () {
+      axios.post('http://localhost:3000/heroList').then(response => {
+          console.log(response);
+          if(response.data.message=="success"){
+            if(response.data.result.length>0){
+              for(var i=0; i<response.data.result.length;i++){
+                this.images.push(response.data.imgURL);
+                this.heroName.push(response.data.result[i].name);
+              }
+
+            }
+          }
+
+      })
+    },
+    methods: {
+      hero () {
+        axios.post('http://localhost:3000/hero').then(response => {
+          if(response.data.message=="success"){
+              console.log(response);
+              this.images.push(response.data.imgURL);
+              this.heroName.push(response.data.heroName);
+          }else{
+            console.log(response.data.message);
+          }
+        })
+      },
+      fight () {
+        axios.post('http://localhost:3000/fight').then(response => {
+          if(response.data.message=="success"){
+              this.fighterImage1=response.data.imgURL;
+              this.fighterImage2=response.data.imgURL;
+              this.hero1=response.data.hero1Name;
+              this.hero2=response.data.hero12Name;
+              this.fightText="Fighting...."
+              this.timer=setInterval(this.winFunction,2000);
+          }else{
+            console.log(response.data.message);
+          }
+        })
+      },
+      champion () {
+        axios.post('http://localhost:3000/champion').then(response => {
+            if(response.data.message=="unsuccess"){
+              alert("Fight Required- "+response.data.length+" Hero's Alive");
+            }else{
+              alert("Winner is- "+response.data.name);
+            }
+        });
+      },
+      winFunction(){
+        clearInterval(this.timer);
+        var winnerNumber=Math.floor(Math.random() * 2)+1;
+        console.log(winnerNumber);
+        var winner_name,loser_name;
+        if(winnerNumber==1){
+          winner_name=this.hero1;
+          loser_name=this.hero2;
+        }else {
+          winner_name=this.hero2;
+          loser_name=this.hero1;
+        }
+        axios.get('http://localhost:3000/winner?winner_name='+winner_name+'&loser_name='+loser_name).then(response => {
+          if(response.data.message=="success"){
+            this.fighterImage1="";
+            this.fighterImage2="";
+            this.hero1="";
+            this.hero2="";
+            this.fightText="";
+            location.reload();
+          }else{
+            console.log(response.data.message);
+          }
+        })
+      },
+    }
+  }
+</script>
+
+<style>
+
+</style>
